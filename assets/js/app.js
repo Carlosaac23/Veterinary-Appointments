f7350fc068c6ff19d6917fefe663a5ad85253207
Carlos <carlosaac232001@gmail.com>
2025-08-13 10:47:38 -0500

    assets(js): add new app entry point module

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+import {
+  pacienteInput,
+  propietarioInput,
+  emailInput,
+  fechaInput,
+  sintomasInput,
+  form,
+} from './selectores.js';
+import { datosCita, submitCita } from './funciones.js';
+
+// Events
+pacienteInput.addEventListener('change', datosCita);
+propietarioInput.addEventListener('change', datosCita);
+emailInput.addEventListener('change', datosCita);
+fechaInput.addEventListener('change', datosCita);
+sintomasInput.addEventListener('change', datosCita);
+form.addEventListener('submit', submitCita);
